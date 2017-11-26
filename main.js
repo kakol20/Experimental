011489d0f24225431356dc6e8391eb8fd2c24f9a
Adrian Aquino-Neary <adrianaquino.aquino33@gmail.com>
2017-11-26 02:18:12 +0000

    2 Changes
    - Added timePast() function
    - Updated TODO List

--- main.js
+++ main.js
@@ -123,6 +123,8 @@ var key = (function() {
         },
 
         //jQuery Needed
+        // https://stackoverflow.com/questions/19395257/how-to-count-duplicate-value-in-an-array-in-javascript
+        // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/for...in
         removeDupes: function(a) {
             var b = [];
             $.each(a, function(i, c) {
@@ -467,6 +469,79 @@ var timeUntil = function() {
     }
     */
     console.log(t >= 1000 ? "Took: " + key.round(t / 1000, "nearest", 4) + "s" : "Took: " + t.toPrecision(5) + "ms");
+    console.log(" ");
+};
+
+var timePast = function() {
+    var inWeeks = function(a, b) {
+        var c = a.getTime();
+        var d = b.getTime();
+        return (c - d) / (7 * 24 * 60 * 60 * 1000);
+    };
+    var convertRound = function(a) {
+        if (a > 0) {
+            return key.round(a, "down");
+        } else if (a < 0) {
+            return key.round(a, "up");
+        } else {
+            return a;
+        }
+    };
+
+    var isValideDate = function(a) {
+        if (Object.prototype.toString.call(a) !== "[object Date]") {
+            return false;
+        }
+        return !isNaN(a.getTime());
+    };
+
+    var date = document.getElementById('timePastInput').value || "random";
+
+    var pastDate = new Date(date);
+    var currentDate = new Date();
+
+    if (!isValideDate(pastDate) || pastDate.getTime() >= currentDate.getTime()) {
+        var tempDate = new Date();
+
+        var month = tempDate.getMonth() + 1;
+        if (month <= 9) {
+            month = "0" + month;
+        }
+
+        var days = tempDate.getDate();
+        if (days <= 9) {
+            days = "0" + days;
+        }
+
+        var year = tempDate.getFullYear() - 1;
+
+        var hours = tempDate.getHours();
+        if (hours <= 9) {
+            hours = "0" + hours;
+        }
+
+        var minutes = tempDate.getMinutes();
+        if (minutes <= 9) {
+            minutes = "0" + minutes;
+        }
+
+        var tempDate1 = month + " " + days + ", " + year + " " + hours + ":" + minutes;
+        tempDate1 = new Date(tempDate1);
+
+        var randomisedDate = key.random(currentDate.getTime(), tempDate1.getTime());
+
+        pastDate = new Date(randomisedDate);
+    }
+
+    var t0 = performance.now();
+
+    currentDate = new Date();
+    var output = key.convertWeek(inWeeks(currentDate, pastDate)) + " since " + pastDate;
+    document.getElementById('timePastOutput').innerHTML = output;
+
+    var t1 = performance.now();
+    var dif = Math.abs(t1 - t0);
+    console.log(dif >= 1000 ? "Took: " + key.round(dif / 1000, "nearest", 4) + "s" : "Took: " + dif.toPrecision(5) + "ms");
     console.log(" ");
 };
 
@@ -1214,8 +1289,10 @@ var lcmAtoB = function() {
 
 /*
 TODO List - 
-1. Enhancement {
-    a. None
-}
-2. None
+1. Implement new count duplicate method
+    a. Now pushes [x, y] (where x = the value & y = the count in each index) to the array output
+    b. This makes it more easier to use
+    c. Functions that use countDupes()
+        i. Averages
+        ii. Iterations
 */
