b90a1178d7a3735e2cfabfc5f2eb3fc0265ce78a
Adrian <adrianaquino.aquino33@gmail.com>
2021-03-12 17:59:48 +0000

    Organise functions

---
+++ scripts/kspTools.js
@@ -0,0 +1,23 @@
+var gravConverter = function()
+{
+	var a = document.getElementById('gravNum').value || 1;
+	var b = document.getElementById('gravPow').value || 1;
+
+	document.getElementById('gravOutput').innerHTML = a * Math.pow(10, b - 9);
+}
+
+var calculateAltitude = function()
+{
+	var time = document.getElementById("orbitTime").value * 60 || 5400;
+	var mu = document.getElementById("orbitPara").value || 3531.6;
+	var radius = document.getElementById("orbitRadius").value || 600;
+
+	// https://forum.kerbalspaceprogram.com/index.php?/topic/64230-calculating-altitude-for-given-orbital-period/
+	// 	a = ((T/2pi)^2 * mu)^1/3
+
+	// That's how I have it in my quick orbit calculator.
+
+	// Note, however, that a is the semi-major axis, not the orbital height. For that you have to substract the radius of the body you are orbiting.
+
+	document.getElementById('orbitOutput').innerHTML = Math.pow(Math.pow(time / (2 * Math.PI), 2) * mu, 1 / 3) - radius;
+}
