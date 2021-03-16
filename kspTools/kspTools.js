92bbda1ba8df587aa1f27dccbe8d4f045b716689
Adrian <adrianaquino.aquino33@gmail.com>
2021-03-16 21:01:14 +0000

    Use decimal.js for precision
    And use big decimal for output

--- kspTools/kspTools.js
+++ kspTools/kspTools.js
@@ -5,23 +5,27 @@ var gravConverter = function()
 
 	b -= 9;
 
+	var aD = new Decimal(a);
+
 	if (b > 0)
 	{
 		for (var i = 0; i < b; i++)
 		{
-			a *= 10;
+			aD = aD.mul(10);
 		}
 	}
 	else if (b < 0)
 	{
 		for (var i = 0; i < Math.abs(b); i++)
 		{
-			a /= 10;
+			aD = aD.div(10);
 		}
 	}
 
+	// console.log()
+
 	// document.getElementById('gravOutput').innerHTML = a * Math.pow(10, b - 9);
-	document.getElementById('gravOutput').innerHTML = a + ' km&sup3/s&sup2';
+	document.getElementById('gravOutput').innerHTML = bigDecimal.getPrettyValue(aD.toString()) + ' km&sup3/s&sup2';
 }
 
 var calculateAltitude = function()
@@ -29,9 +33,26 @@ var calculateAltitude = function()
 	// var time = document.getElementById('orbitTime').value * 60 || 5400;
 	var time = (document.getElementById('orbitHours').value || 0) * 60 * 60;
 	time += (document.getElementById('orbitMinutes').value || 0) * 60;
-	time += document.getElementById('orbitSeconds').value || 0;
+	time += document.getElementById('orbitSeconds').value || 1;
 	var mu = document.getElementById('orbitPara').value || 3531.6;
 	var radius = document.getElementById('orbitRadius').value || 600;
+
+	// https://github.com/MikeMcl/decimal.js/
+	var tau = new Decimal(Math.PI);
+	tau = tau.mul(2);
+
+	var result = new Decimal(time);
+	result = result.div(tau);
+	result = result.mul(result);
+	result = result.mul(mu);
+
+	var third = new Decimal(1);
+	third = third.div(3);
+
+	result = result.pow(third);
+	result = result.sub(radius);
+
+	// console.log();
 
 	// https://forum.kerbalspaceprogram.com/index.php?/topic/64230-calculating-altitude-for-given-orbital-period/
 	// 	a = ((T/2pi)^2 * mu)^1/3
@@ -39,6 +60,7 @@ var calculateAltitude = function()
 	// That's how I have it in my quick orbit calculator.
 
 	// Note, however, that a is the semi-major axis, not the orbital height. For that you have to substract the radius of the body you are orbiting.
+	console.log(bigDecimal.getPrettyValue(result.toString()));
 
-	document.getElementById('orbitOutput').innerHTML = (Math.pow(Math.pow(time / (2 * Math.PI), 2) * mu, 1 / 3) - radius) + ' km';
+	document.getElementById('orbitOutput').innerHTML = bigDecimal.getPrettyValue(result.toDecimalPlaces(4)) + ' km';
 }
