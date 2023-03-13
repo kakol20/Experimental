87b541aa4a8c7f26b17bfc7e1b1e2005b0f2edcd
Adrian Aquino-Neary <adrianaquino.aquino33@gmail.com>
2023-03-13 21:02:10 +0000

    Style input

--- ksp/main.js
+++ ksp/main.js
@@ -1,62 +1,62 @@
 ﻿class Body {
-	constructor(sgp, sgpPow, radius, rotPeriod) {
-		this.sgp = sgp * Math.pow(10, sgpPow - 9);
+    constructor(sgp, sgpPow, radius, rotPeriod) {
+        this.sgp = sgp * Math.pow(10, sgpPow - 9);
 
-		this.radius = radius; // in km
-		this.rotPeriod = rotPeriod; // in seconds
-	}
+        this.radius = radius; // in km
+        this.rotPeriod = rotPeriod; // in seconds
+    }
 };
 
 const tools = (function () {
-	return {
-		updateBody: function () {
-			this.orbitBody = document.getElementById("orbitBody").value || "kerbin";
+    return {
+        updateBody: function () {
+            this.orbitBody = document.getElementById("orbitBody").value || "kerbin";
 
-			console.log("Orbit Body changed to: " + this.orbitBody);
-		},
-		orbitBody: "kerbin",
+            console.log("Orbit Body changed to: " + this.orbitBody);
+        },
+        orbitBody: "kerbin",
 
-		bodies: new Map(),
+        bodies: new Map(),
 
-		getBody: function () {
-			return this.bodies.get(this.orbitBody);
-		},
+        getBody: function () {
+            return this.bodies.get(this.orbitBody);
+        },
 
-		semiMajorAxis: function (apoapsis, periapsis) {
-			return (apoapsis + periapsis + (tools.getBody().radius * 2)) / 2;
-		},
+        semiMajorAxis: function (apoapsis, periapsis) {
+            return (apoapsis + periapsis + (tools.getBody().radius * 2)) / 2;
+        },
 
-		orbitalPeriod: function (sma) {
-			return (Math.PI * 2) * Math.sqrt(Math.pow(sma, 3) / tools.getBody().sgp);
-		},
+        orbitalPeriod: function (sma) {
+            return (Math.PI * 2) * Math.sqrt(Math.pow(sma, 3) / tools.getBody().sgp);
+        },
 
-		cleanPeriod: function (period) {
-			let accumulated = period;
-			let output = "";
+        cleanPeriod: function (period) {
+            let accumulated = period;
+            let output = "";
 
-			const seconds = accumulated % 60;
-			accumulated -= seconds;
-			accumulated /= 60;
+            const seconds = accumulated % 60;
+            accumulated -= seconds;
+            accumulated /= 60;
 
-			const minutes = accumulated % 60;
-			accumulated -= minutes;
-			accumulated /= 60;
+            const minutes = accumulated % 60;
+            accumulated -= minutes;
+            accumulated /= 60;
 
-			output = String(accumulated) + "h, ";
-			output += String(minutes) + "m, ";
-			output += Decimal(seconds).toDecimalPlaces(4) + "s";
+            output = String(accumulated) + "h, ";
+            output += String(minutes) + "m, ";
+            output += Decimal(seconds).toDecimalPlaces(4) + "s";
 
-			return output;
-		}
-	};
+            return output;
+        }
+    };
 })();
 
 // Runs script when page is loaded or reloaded
 $(function () {
-	console.log("Script by kakol20");
-	console.log("-----");
-	//console.log("");
+    console.log("Script by kakol20");
+    console.log("-----");
+    //console.log("");
 
-	tools.bodies.set("kerbin", new Body(3.5316, 12, 600, 21549.425));
-	tools.bodies.set("mun", new Body(6.5138398, 10, 200, 138984.38));
+    tools.bodies.set("kerbin", new Body(3.5316, 12, 600, 21549.425));
+    tools.bodies.set("mun", new Body(6.5138398, 10, 200, 138984.38));
 });
