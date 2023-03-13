d5e91362e1e46393165a3a73de0356bffe6162ef
Adrian Aquino-Neary <adrianaquino.aquino33@gmail.com>
2023-03-13 11:35:41 +0000

    Add orbital period calculation

--- ksp/main.js
+++ ksp/main.js
@@ -1,12 +1,9 @@
 ﻿class Body {
 	constructor(sgp, sgpPow, radius, rotPeriod) {
-		this.sgp = new Decimal(sgp);
-		let pow = new Decimal(10).pow(sgpPow - 9);
+		this.sgp = sgp * Math.pow(10, sgpPow - 9);
 
-		this.sgp = this.sgp.mul(pow);
-
-		this.radius = new Decimal(radius); // in km
-		this.rotPeriod = new Decimal(rotPeriod); // in seconds
+		this.radius = radius; // in km
+		this.rotPeriod = rotPeriod; // in seconds
 	}
 };
 
@@ -19,7 +16,38 @@ const tools = (function () {
 		},
 		orbitBody: "kerbin",
 
-		bodies: new Map()
+		bodies: new Map(),
+
+		getBody: function () {
+			return this.bodies.get(this.orbitBody);
+		},
+
+		semiMajorAxis: function (apoapsis, periapsis) {
+			return (apoapsis + periapsis + (tools.getBody().radius * 2)) / 2;
+		},
+
+		orbitalPeriod: function (sma) {
+			return (Math.PI * 2) * Math.sqrt(Math.pow(sma, 3) / tools.getBody().sgp);
+		},
+
+		cleanPeriod: function (period) {
+			let accumulated = period;
+			let output = "";
+
+			const seconds = accumulated % 60;
+			accumulated -= seconds;
+			accumulated /= 60;
+
+			const minutes = accumulated % 60;
+			accumulated -= minutes;
+			accumulated /= 60;
+
+			output = String(accumulated) + "h, ";
+			output += String(minutes) + "m, ";
+			output += Decimal(seconds).toDecimalPlaces(4) + "s";
+
+			return output;
+		}
 	};
 })();
 
@@ -30,5 +58,5 @@ $(function () {
 	//console.log("");
 
 	tools.bodies.set("kerbin", new Body(3.5316, 12, 600, 21549.425));
-	tools.bodies.set("mun", new Body(new Decimal(6.5138398), 10, 200, 138984.38));
+	tools.bodies.set("mun", new Body(6.5138398, 10, 200, 138984.38));
 });
--- ksp/orbitalPeriod.js
+++ ksp/orbitalPeriod.js
@@ -1,4 +1,18 @@
 const orbitalPeriod = function () {
 	console.log("----- CALCULATING ORBITAL PERIOD -----");
+
+	const apoapsis = parseFloat(document.getElementById("orbitPeriodAp").value) || 0;
+	const periapsis = parseFloat(document.getElementById("orbitPeriodPe").value) || 0;
+
+	const semiMajorAxis = tools.semiMajorAxis(apoapsis, periapsis);
+
+	console.log("Apoapsis: " + apoapsis);
+	console.log("Periapsis: " + periapsis);
+	console.log("Semi-major axis: " + semiMajorAxis);
+
+	const result = tools.orbitalPeriod(semiMajorAxis);
+
 	console.log("----- END -----");
+
+	document.getElementById("orbitPeriodOutput").innerHTML = tools.cleanPeriod(result);
 };
